2806c905663a5322f4d01396fb7f5a10d3d4ca58
nanpuyue <nanpuyue@gmail.com>
2019-01-11 21:52:17 +0800

    rename CRC to Crc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(reverse_bits)]
 
-pub struct CRC {
+pub struct Crc {
     crc: u8,
     poly: u8,
     offset: usize,
@@ -10,7 +10,7 @@ pub struct CRC {
     lookup_table: [u8; 256],
 }
 
-impl CRC {
+impl Crc {
     pub fn new(poly: u8, width: usize, initial: u8, final_xor: u8, reflect: bool) -> Self {
         let offset = 8 - width;
         let mut crc = Self {
@@ -96,22 +96,22 @@ mod tests {
         let data0 = b"c4ca4238a0b923820dcc509a6f75849b".as_ref();
         let data1 = b"c81e728d9d4c2f636f067f89cc14862c".as_ref();
 
-        let mut crc5usb = CRC::new(0x05, 5, 0x1f, 0x1f, true);
+        let mut crc5usb = Crc::new(0x05, 5, 0x1f, 0x1f, true);
         assert_eq!(crc5usb.update(data0), 0x17);
         crc5usb.init();
         assert_eq!(crc5usb.update(data1), 0x1c);
 
-        let mut crc5x = CRC::new(0x05, 5, 0x1f, 0x1f, false);
+        let mut crc5x = Crc::new(0x05, 5, 0x1f, 0x1f, false);
         assert_eq!(crc5x.update(data0), 0x06);
         crc5x.init();
         assert_eq!(crc5x.update(data1), 0x02);
 
-        let mut crc8 = CRC::new(0x07, 8, 0, 0, false);
+        let mut crc8 = Crc::new(0x07, 8, 0, 0, false);
         assert_eq!(crc8.update(data0), 0x47);
         crc8.init();
         assert_eq!(crc8.update(data1), 0x45);
 
-        let mut crc8rohc = CRC::new(0x07, 8, 0xff, 0, true);
+        let mut crc8rohc = Crc::new(0x07, 8, 0xff, 0, true);
         assert_eq!(crc8rohc.update(data0), 0x12);
         crc8rohc.init();
         assert_eq!(crc8rohc.update(data1), 0xcb);
