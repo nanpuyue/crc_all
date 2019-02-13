fa0c122313b4998f5d57692a1b01d951b4252dba
nanpuyue <nanpuyue@gmail.com>
2019-02-13 10:54:38 +0800

    use feature `test`

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,8 +1,11 @@
-use bencher::Bencher;
-use bencher::{benchmark_group, benchmark_main};
+#![feature(test)]
 
+extern crate test;
+
+use test::Bencher;
 use crc::Crc;
 
+#[bench]
 fn crc08_update_kilobytes(b: &mut Bencher) {
     let data = Box::new([0u8; 1000]);
     let mut crc8_rohc = Crc::<u16>::new(0x07, 8, 0xff, 0, true);
@@ -12,6 +15,7 @@ fn crc08_update_kilobytes(b: &mut Bencher) {
     });
 }
 
+#[bench]
 fn crc16_update_kilobytes(b: &mut Bencher) {
     let data = Box::new([0u8; 1000]);
     let mut crc16_ibm_sdlc = Crc::<u16>::new(0x1021, 16, 0xffff, 0xffff, true);
@@ -21,6 +25,7 @@ fn crc16_update_kilobytes(b: &mut Bencher) {
     });
 }
 
+#[bench]
 fn crc32_update_kilobytes(b: &mut Bencher) {
     let data = Box::new([0u8; 1000]);
     let mut crc32_iso_hdlc = Crc::<u32>::new(0x04c11db7, 32, 0xffffffff, 0xffffffff, true);
@@ -30,6 +35,7 @@ fn crc32_update_kilobytes(b: &mut Bencher) {
     });
 }
 
+#[bench]
 fn crc64_update_kilobytes(b: &mut Bencher) {
     let data = Box::new([0u8; 1000]);
     let mut crc64_ecma182 = Crc::<u64>::new(0x42f0e1eba9ea3693, 64, 0, 0, false);
@@ -38,12 +44,3 @@ fn crc64_update_kilobytes(b: &mut Bencher) {
         crc64_ecma182.update(&*data);
     });
 }
-
-benchmark_group!(
-    benches,
-    crc08_update_kilobytes,
-    crc16_update_kilobytes,
-    crc32_update_kilobytes,
-    crc64_update_kilobytes
-);
-benchmark_main!(benches);
