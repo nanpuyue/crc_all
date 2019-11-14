9b302b9cf192372c5567bc60751396ab43de4640
南浦月 <nanpuyue@gmail.com>
2019-11-14 17:27:11 +0800

    update `benches/benches.rs`

--- benches/benches.rs
+++ benches/benches.rs
@@ -6,41 +6,76 @@ use crc_all::Crc;
 use test::Bencher;
 
 #[bench]
-fn crc08_update_kilobytes(b: &mut Bencher) {
-    let data = Box::new([0u8; 1000]);
-    let mut crc8_rohc = Crc::<u16>::new(0x07, 8, 0xff, 0, true);
+fn crc08_update_megabytes(b: &mut Bencher) {
+    let mut data = Vec::with_capacity(1000000);
+    unsafe {
+        data.set_len(1000000);
+    }
+
+    let mut crc8_smbus = Crc::<u8>::new(0x07, 8, 0, 0, false);
+
     b.iter(|| {
-        crc8_rohc.init();
-        crc8_rohc.update(&*data);
+        crc8_smbus.init();
+        crc8_smbus.update(&data);
     });
 }
 
 #[bench]
-fn crc16_update_kilobytes(b: &mut Bencher) {
-    let data = Box::new([0u8; 1000]);
+fn crc12_update_megabytes(b: &mut Bencher) {
+    let mut data = Vec::with_capacity(1000000);
+    unsafe {
+        data.set_len(1000000);
+    }
+
+    let mut crc12_umts = Crc::<u16>::new(0x80f, 12, 0, 0, false);
+
+    b.iter(|| {
+        crc12_umts.init();
+        crc12_umts.update(&data);
+    });
+}
+
+#[bench]
+fn crc16_update_megabytes(b: &mut Bencher) {
+    let mut data = Vec::with_capacity(1000000);
+    unsafe {
+        data.set_len(1000000);
+    }
+
     let mut crc16_ibm_sdlc = Crc::<u16>::new(0x1021, 16, 0xffff, 0xffff, true);
+
     b.iter(|| {
         crc16_ibm_sdlc.init();
-        crc16_ibm_sdlc.update(&*data);
+        crc16_ibm_sdlc.update(&data);
     });
 }
 
 #[bench]
-fn crc32_update_kilobytes(b: &mut Bencher) {
-    let data = Box::new([0u8; 1000]);
+fn crc32_update_megabytes(b: &mut Bencher) {
+    let mut data = Vec::with_capacity(1000000);
+    unsafe {
+        data.set_len(1000000);
+    }
+
     let mut crc32_iso_hdlc = Crc::<u32>::new(0x04c11db7, 32, 0xffffffff, 0xffffffff, true);
+
     b.iter(|| {
         crc32_iso_hdlc.init();
-        crc32_iso_hdlc.update(&*data);
+        crc32_iso_hdlc.update(&data);
     });
 }
 
 #[bench]
-fn crc64_update_kilobytes(b: &mut Bencher) {
-    let data = Box::new([0u8; 1000]);
+fn crc64_update_megabytes(b: &mut Bencher) {
+    let mut data = Vec::with_capacity(1000000);
+    unsafe {
+        data.set_len(1000000);
+    }
+
     let mut crc64_ecma182 = Crc::<u64>::new(0x42f0e1eba9ea3693, 64, 0, 0, false);
+
     b.iter(|| {
         crc64_ecma182.init();
-        crc64_ecma182.update(&*data);
+        crc64_ecma182.update(&data);
     });
 }
